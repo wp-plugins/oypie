30a6d01035cd664717d1f10378f67d4c17830f6a
sanderonline <sanderonline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-20 19:00:59 +0000

    First edition of OYPie
    git-svn-id: https://plugins.svn.wordpress.org/oypie/trunk@918117 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== OYPie ===
+Contributors: Sander Dijkstra
+Donate link: http://sanderonlinemedia.nl/
+Tags: oypo, albums, photobrowser, fotobrowser, reselling, photos, photoalbum,
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+This plugin is for photographers who use the service 'OYPO'. In this plugin you can generate shortcodes for your pages and posts. 
+The shortcode generator can you find under 'Tools' > 'SanderOnline'.
+
+== Installation ==
+
+1. Install the plugin from the plugin database.
+2. Activate the plugin
+
+And start with the shortcodes!
+
+== Frequently Asked Questions ==
+
+= Where can i find the shortcode generator? =
+
+The shortcode generator can you find under 'Tools' -> 'SanderOnline'
+
+
+== Changelog ==
+
+= 0.1 = 
+Released at 07 May 2014
+
+* First version of OYPie.
+* Shorttags and generator added
---
+++ shortcode.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * SHORTCODE OUTPUT
+ */
+function oypo_output($atts) {
+   extract(shortcode_atts(array(
+      'type' => 'user',
+      'id' => 'sanderonline',
+      'wl' => '',
+      'trans' => '1',
+      'nonav' => '1',
+      'css' => ''
+   ), $atts));
+   
+   //Mapid or userid
+   if($type == 'map'){
+    $out_id = "var mapid='".$id."';";
+   }elseif($type == 'user'){
+    $out_id = "var userid='".$id."';";    
+   }elseif($type == 'school'){
+    $out_id = "";
+   }
+   
+   //Transparcency
+   if($trans == 1) {
+    $out_trans = "var transparency=1;";
+   }else{
+    $out_trans = "";
+   }
+   //Whitelable sending
+   if($wl != ""){
+    $out_wl = "var wl='".$wl."';.";
+   }
+   //No mapnavigation
+   if($nonav == 0 && $type == 'map' ){
+    $out_nav = "var nonav=0;";
+   }elseif($nonav == 1 &&  $type == 'map'){
+    $out_nav = "var nonav=1;";
+   }else{
+    $out_nav = "";
+   }
+   //Custom css link
+   if($css != NULL){;
+    $array = get_headers($css);
+    $string = $array[0];
+    if(strpos($string,"200")) {
+        $out_css = "var css='".$css."';";
+    } else {
+        $out_css = "";
+    }
+    }else{
+        $out_css = "";
+    }
+
+   
+$output = "
+        <script type=\"text/javascript\">
+        var mode='" .$type."';
+        ". $out_id ."
+        ". $out_trans ."
+        ". $out_wl."
+        ". $out_nav ."
+        ". $out_css ."
+        </script>
+        <script type=\"text/javascript\" src=\"".plugins_url()."/oypie/js/oypie.js\"></script>
+        <div id=\"pixxer_iframe\"></div>";
+        
+        return $output;
+}
+
+
+add_shortcode('oypo', 'oypo_output');?>
